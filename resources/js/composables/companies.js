514e253619936e781ed6fa37741811c4ddb41496
Invullen <Invullen@iMac-van-Invullen.local>
2022-08-30 17:04:48 +0300

    complete the create and store form and its logic

--- resources/js/composables/companies.js
+++ resources/js/composables/companies.js
@@ -1,31 +1,53 @@
 import axios from "axios"
 import { ref } from "vue"
+import { useRouter } from "vue-router";
 
 export default function useCompanies() {
 
-    const companies = ref([])// this makes it reactive/dynamic
+    // this makes it reactive/dynamic
+    const companies = ref([])
+    const router = useRouter();
+    const  errors= ref('')
+
 
     // get companies method/function
     const getCompanies = async () => {
         let response = await axios.get('/api/companies')
         companies.value = response.data.data;
     }
+    // store/method
+    // const storeCompany = async (data) => {
+    //     console.log(data);
+    //     await axios.post('/api/companies', data)
+    //     await router.push({name: 'companies.index'})
+    // }
+
+    const storeCompany = async (data) => {
+        errors.value = ''
+        try {
+            await axios.post('/api/companies', data)
+            await router.push({name: 'companies.index'})
+        } catch (e) {
+            if (e.response.status === 422) {
+                errors.value = e.response.data.errors
+            }
+        }
+    }
+
 
     // destroy/delete method
     const destroyCompany = async (id) => {
         await axios.delete('/api/companies/' + id)
     }
-    // // create/delete method
-    // const createCompany = async (id) => {
-    //     await axios.delete('/api/companies/' + id)
-    // }
     // // update/delete method
     // const updateCompany = async (id) => {
     //     await axios.delete('/api/companies/' + id)
     // }
     return {
         companies,
+        errors,
         getCompanies,
+        storeCompany,
         destroyCompany
     }
 }
